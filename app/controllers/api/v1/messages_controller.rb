4a86eaadc56f6f17dc396a2c6b8bcb6809ef2a20
github username <etellezp@gmail.com>
2019-08-03 00:50:38 -0400

    Added fast json api serializer gem, create user and message serializers

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::MessagesController < ApplicationController
 
   def index
     @messages = Message.all
-    render json: @messages, status: :ok
+    json_messages = MessageSerializer.new(@messages).serialized_json
+    render json: json_messages, status: :ok
   end
 
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users, status: :ok
+    json_users = UserSerializer.new(@users).serialized_json
+    render json: json_users, status: :ok
   end
 
 end
