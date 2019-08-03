d809bce13c8c1e4ea91ea7bed8c6672525794de1
github username <etellezp@gmail.com>
2019-08-03 18:14:45 -0400

    Added create session, destroy session and get current user in sessions controller, also added helper methods in application controller to get for current user and if a user is logged in

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      render json: @user
+      user_json = UserSerializer.new(@users).serialized_json
+      render json: user_json, status: :ok
     else
       render json: {
         error: "Invalid Credentials"
@@ -13,6 +14,20 @@ class Api::V1::SessionsController < ApplicationController
   end
 
   def delete
+    session.clear
+    render json: {
+      notice: "Logged Out"
+    }, status: :ok
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: UserSerializer.new(current_user).serialized_json
+    else
+      render json: {
+        error: "No user is logged in"
+      }
+    end
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::API
-  include ::ActionController::Cookies 
+  include ::ActionController::Cookies
 
   def current_user
-    User.find(session[:user_id])
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
   end
 end
